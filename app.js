0e533511b97f30c126dd20a805b8d27f8e70c660
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-08-29 15:19:59 +0200

    change to get mountains/:id

--- app.js
+++ app.js
@@ -2,43 +2,59 @@ const express = require("express");
 
 const app = express();
 
-
 const mountains = [
-    {
-        name: "Andes",
-        height: 6960,
-        location: "South America"
-    },
-    {
-        name: "Himalaya",
-        height: 8848,
-        location: "Asia"
-    },
-    {
-        name: "Rocky Mountains",
-        height: 4392,
-        location: "North America"
-    }
+  {
+    id: 1,
+    name: "Andes",
+    height: 6960,
+    location: "South America",
+  },
+  {
+    id: 2,
+    name: "Himalaya",
+    height: 8848,
+    location: "Asia",
+  },
+  {
+    id: 3,
+    name: "Rocky Mountains",
+    height: 4392,
+    location: "North America",
+  },
 ];
 
-
 app.get("/mountains", (req, res) => {
-    res.send(mountains);
+
+  res.send(mountains);
+
 });
 
-
+// Below code finds mountain by assigned id.
 app.get("/mountains/:id", (req, res) => {
-    
+
+  const id = parseInt(req.params.id);
+
+  // Find mountain by id
+  const mountain = mountains.find(mountain => mountain.id === id);
+
+  if (!mountain) {
+    res.send({ message: "No mountain with that id." });
+  } else {
+    res.send(mountain);
+  }
+});
+
+// Below code finds mountain by index in the Array.
+/* app.get("/mountains/:id", (req, res) => {
+  
     const id = req.params.id;
 
-    // If statements to check if input is a number and if the id exists.
-    if (isNaN(id)) {
-        res.send({ message: "Invalid ID. Input must be a number." });
-    } else if(id >= mountains.length){
-        res.send({ message: `The ID ${id} does not exist.` });
-    } else {
-        res.send(mountains[id]);
-    }
-})
+  // If statements to check if input is a number and if the id exists.
+  if (isNaN(id) || id >= mountains.length) {
+    res.send({ message: `Invalid ID. Input must be a number within valid range (0-${mountains.length - 1}).` });
+  } else {
+    res.send(mountains[id]);
+  }
+}); */
 
 app.listen(8080);
